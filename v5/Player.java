39f0441a6e95df72b8566d6aba4797d636f9e0eb
Brianna <btieu30@stuy.edu>
2022-01-20 01:48:21 -0500

    v5_horizontal + vertical

---
+++ v5/Player.java
@@ -0,0 +1,17 @@
+public class Player {
+
+  protected String _color;
+
+  public Player() {
+    _color = "R";
+  }
+
+  public void setColor( String newColor ) {
+    _color = newColor;
+  }
+
+  public String color() {
+    return _color;
+  }
+
+}
