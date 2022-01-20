10b49138aa34cd323e2e3216d7780d9f5eba8825
Brianna <btieu30@stuy.edu>
2022-01-19 22:01:31 -0500

    v3

---
+++ v3/Player.java
@@ -0,0 +1,13 @@
+public class Player {
+
+  protected String _color;
+
+  public void setColor( String newColor ) {
+    _color = newColor;
+  }
+
+  public String color() {
+    return _color;
+  }
+
+}
